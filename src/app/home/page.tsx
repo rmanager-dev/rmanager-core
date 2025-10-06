0bd9166cfbe5af75a4900301171818c951f0b7b1
OverDsh <valartcode@gmail.com>
2025-10-06 18:14:55 +0200

    Style: Added dotted background

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -3,7 +3,7 @@ import HomepageHeader from "./homeComponents/Header";
 
 export default function Page() {
   return (
-    <div className="bg-linear-to-b from-background to-popover min-h-screen w-full ">
+    <div className="bg-[radial-gradient(var(--secondary),transparent_1px)] [background-size:16px_16px] h-full w-full">
       <div className="min-h-screen w-full p-20 flex flex-col items-center gap-20">
         <HomepageHeader />
         <HomepageFeatureCards />
