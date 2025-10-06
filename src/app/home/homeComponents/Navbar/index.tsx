5080dcf524063aecf2bb041370d5e3eb89beb5f4
OverDsh <valartcode@gmail.com>
2025-10-06 17:19:14 +0200

    Fix: Invert logo color for light theme

--- src/app/home/homeComponents/Navbar/index.tsx
+++ src/app/home/homeComponents/Navbar/index.tsx
@@ -7,7 +7,13 @@ export default function Navbar() {
     <div className="flex bg-background w-full z-40 h-18 justify-center border-b-1">
       <div className="w-full max-w-[100rem] px-5 lg:px-16 xl:px-20 flex justify-between items-center">
         <div className="flex">
-          <Image src={"brand/logo.svg"} width={80} height={80} alt="Logo" />
+          <Image
+            src={"brand/logo.svg"}
+            width={80}
+            height={80}
+            alt="Logo"
+            className="invert dark:invert-0"
+          />
         </div>
         <RightOptions />
       </div>
