5ac6d09ca2de710031e8aaa7754d8ac42cee5dd5
OverDsh <valartcode@gmail.com>
2025-10-06 17:34:32 +0200

    style: add backdrop blur effect to navbar

--- src/app/home/homeComponents/Navbar/index.tsx
+++ src/app/home/homeComponents/Navbar/index.tsx
@@ -4,7 +4,7 @@ import RightOptions from "./RightOptions";
 
 export default function Navbar() {
   return (
-    <div className="flex bg-background w-full z-40 h-18 justify-center border-b-1">
+    <div className="flex backdrop-blur-3xl w-full z-40 h-18 justify-center border-b-1">
       <div className="w-full max-w-[100rem] px-5 lg:px-16 xl:px-20 flex justify-between items-center">
         <div className="flex">
           <Image
