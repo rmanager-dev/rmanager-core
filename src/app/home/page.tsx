d767c7a1bfae4761c52da68c98566b185b94393a
OverDsh <valartcode@gmail.com>
2025-10-06 19:59:11 +0200

    Style: Decreased padding arround homepage elements

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -4,7 +4,7 @@ import HomepageHeader from "./components/HeaderText";
 export default function Page() {
   return (
     <div className="bg-[radial-gradient(var(--secondary),transparent_1px)] [background-size:16px_16px] h-full w-full">
-      <div className="min-h-screen w-full p-20 flex flex-col items-center gap-20">
+      <div className="min-h-screen w-full p-15 flex flex-col items-center gap-20">
         <HomepageHeader />
         <HomepageFeatureCards />
       </div>
