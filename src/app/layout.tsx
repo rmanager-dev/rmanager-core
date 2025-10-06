840c99e56a22c8497e03701f11f82efdf51490d7
OverDsh <valartcode@gmail.com>
2025-10-06 18:42:45 +0200

    Style: Added Geist font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,10 +2,16 @@ import "@/src/app/globals.css";
 import { ThemeProvider } from "next-themes";
 import ThemedToaster from "../components/ThemedToaster";
 import AuthProvider from "../components/AuthProvider";
+import { GeistSans } from "geist/font/sans";
+import { GeistMono } from "geist/font/mono";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html
+      lang="en"
+      suppressHydrationWarning
+      className={`${GeistSans.variable} ${GeistMono.variable}`}
+    >
       <body className="font-sans">
         <AuthProvider>
           <ThemeProvider
