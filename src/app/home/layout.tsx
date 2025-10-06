13c45ebae4a583e8b530c55a5f825c62f37d10bd
OverDsh <valartcode@gmail.com>
2025-10-06 18:14:37 +0200

    Fix: Removed useless styling

--- src/app/home/layout.tsx
+++ src/app/home/layout.tsx
@@ -2,8 +2,7 @@ import Navbar from "./homeComponents/Navbar";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex-col min-h-screen">
-      {/* Sidebar */}
+    <div>
       <header className="sticky top-0">
         <Navbar />
       </header>
