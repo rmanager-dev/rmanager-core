efdc712fb19b62f608c660f206a6baf0cef26ec6
OverDsh <valartcode@gmail.com>
2025-10-07 20:08:13 +0200

    Enhancemnt: Redirect user to homepage on logout

--- src/app/home/components/Navbar/Buttons.tsx
+++ src/app/home/components/Navbar/Buttons.tsx
@@ -38,7 +38,7 @@ export default function RightOptions() {
   const logoutUser = async () => {
     signOut(auth);
     await revokeSessionCookie();
-    router.push("/login");
+    router.push("/home");
   };
 
   const GithubButton = () => {
