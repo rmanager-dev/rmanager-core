4737ad6e78eb652afb0d388d24401758d7514fc3
OverDsh <valartcode@gmail.com>
2025-10-15 19:08:16 +0200

    Refactor: Renamed HomepageHeader component

--- src/app/home/components/HomepageHeader.tsx
+++ src/app/home/components/HomepageHeader.tsx
@@ -5,13 +5,13 @@ import Link from "next/link";
 export default function HomepageHeader() {
   return (
     <div className="w-full flex flex-col items-center gap-6 text-center">
-      <h1 className="text-foreground text-balance text-5xl lg:text-7xl font-semibold max-w-4xl">
-        Manage Your Roblox Deployments with Ease
+      <h1 className="text-foreground text-balance text-5xl md:text-6xl lg:text-7xl font-semibold max-w-4xl">
+        Manage your Roblox Deployments Easily
       </h1>
-      <p className="text-foreground text-balance text-sm lg:text-xl max-w-3xl">
-        rManager lets you create isolated environments where you can schedule
-        releases, restrict access and automate luau tasks. Everything for free,
-        everything Open Source.
+      <p className="text-foreground text-balance text-sm md:text-lg lg:text-xl max-w-3xl">
+        rManager is your Vercel for Roblox: create isolated environments to
+        deploy, schedule, and automate your releases easily - and much more.
+        Everything for free, all open source
       </p>
       <div className="flex justify-center w-full">
         <Button size={"lg"} asChild>
