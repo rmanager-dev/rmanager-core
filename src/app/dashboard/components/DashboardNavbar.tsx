44e532a38282cf57103daefbfca0e00c30d5c33b
OverDsh <valartcode@gmail.com>
2025-11-02 14:09:27 +0100

    Feat: Created the dashboard navbar

---
+++ src/app/dashboard/components/DashboardNavbar.tsx
@@ -0,0 +1,27 @@
+import Navbar from "@/src/components/Navbar";
+import Link from "next/link";
+import Image from "next/image";
+import UserDropdown from "@/src/components/Navbar/UserDropdown";
+import { SidebarTrigger } from "@/src/components/ui/sidebar";
+
+export default function DashboardNavbar() {
+  return (
+    <Navbar className="h-14">
+      <div className="w-full flex justify-between items-center px-2">
+        <div className="flex items-center gap-2">
+          <SidebarTrigger className="md:hidden" />
+          <Link href={"/dashboard"}>
+            <Image
+              src={"brand/logo.svg"}
+              width={60}
+              height={60}
+              alt="Logo"
+              className="invert dark:invert-0"
+            />
+          </Link>
+        </div>
+        <UserDropdown triggerProps={{ className: "rounded-full" }} />
+      </div>
+    </Navbar>
+  );
+}
--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,9 +1,16 @@
 import ProtectedRoute from "@/src/components/ProtectedRoute";
+import DashboardNavbar from "./components/DashboardNavbar";
+import { SidebarProvider } from "@/src/components/ui/sidebar";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <ProtectedRoute rules={["requireLoggedIn"]} fallbackRoute="/login">
-      {children}
+      <SidebarProvider className="flex flex-col">
+        <header className="sticky top-0 flex shrink-0">
+          <DashboardNavbar />
+        </header>
+        <main className="flex-1 overflow-auto">{children}</main>
+      </SidebarProvider>
     </ProtectedRoute>
   );
 }
