3db3cae94bf19fba4a9f88840de32ac149d9e672
OverDsh <overdshdev@gmail.com>
2025-12-14 20:58:33 +0100

    Feat: Wrap layout in QueryClientProvider

---
+++ src/components/QueryClientWrapper.tsx
@@ -0,0 +1,13 @@
+"use client";
+import React from "react";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient();
+
+export default function QueryClientWrapper({
+  children,
+}: React.PropsWithChildren) {
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
+}
