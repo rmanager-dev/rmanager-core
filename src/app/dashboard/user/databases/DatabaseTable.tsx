a16e6ec00231328445f14c7786f51f70669da94c
OverDsh <overdshdev@gmail.com>
2025-12-14 17:37:50 +0100

    Enhancement: Made database table headers bold

--- src/app/dashboard/user/databases/DatabaseTable.tsx
+++ src/app/dashboard/user/databases/DatabaseTable.tsx
@@ -58,7 +58,7 @@ export function DatabaseTable<TData, TValue>({
             {table.getHeaderGroups().map((headerGroup) => (
               <TableRow key={headerGroup.id}>
                 {headerGroup.headers.map((header) => (
-                  <TableHead key={header.id}>
+                  <TableHead key={header.id} className="font-bold">
                     {header.isPlaceholder
                       ? null
                       : flexRender(
