48a847607b817ac4af94c36f969624c8059ec989
OverDsh <overdshdev@gmail.com>
2026-01-14 19:29:39 +0100

    Feat: Created reset password page

---
+++ src/app/reset-password/SetNewPassword.tsx
@@ -0,0 +1,135 @@
+import { Button } from "@/src/components/ui/button";
+import {
+  Empty,
+  EmptyContent,
+  EmptyDescription,
+  EmptyHeader,
+  EmptyTitle,
+} from "@/src/components/ui/empty";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/src/components/ui/form";
+import { Input } from "@/src/components/ui/input";
+import { authClient } from "@/src/lib/auth-client";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { toast } from "sonner";
+import z from "zod";
+
+interface SetNewPasswordProps {
+  token: string;
+}
+export default function SetNewPassword({ token }: SetNewPasswordProps) {
+  const [isLoading, setIsLoading] = useState(false);
+  const router = useRouter();
+
+  const formSchema = z
+    .object({
+      newPassword: z
+        .string()
+        .min(6, { error: "Password must be at least 6 characters" })
+        .max(256, { error: "Password must not have more than 256 characters" }),
+
+      confirmPassword: z.string(),
+    })
+    .refine((data) => data.newPassword == data.confirmPassword, {
+      error: "Passwords do not match",
+      path: ["confirmPassword"],
+    });
+
+  const form = useForm({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      newPassword: "",
+      confirmPassword: "",
+    },
+  });
+
+  const handlePasswordReset = async (newPassword: string) => {
+    setIsLoading(true);
+    const toastId = toast.loading("Resetting your password...");
+    const { error } = await authClient.resetPassword({
+      newPassword,
+      token,
+    });
+
+    if (error) {
+      toast.error(error.message, { id: toastId });
+    } else {
+      toast.success("Successfully reset your password!", { id: toastId });
+    }
+
+    setIsLoading(false);
+    router.push("/sign-in");
+  };
+
+  return (
+    <div className="flex min-h-dvh justify-center items-center">
+      <Empty className="w-full p-10">
+        <EmptyHeader>
+          <EmptyTitle>Reset your password</EmptyTitle>
+          <EmptyDescription>
+            We will send you a link to reset your password
+          </EmptyDescription>
+        </EmptyHeader>
+        <EmptyContent>
+          <Form {...form}>
+            <form
+              className="w-full flex flex-col gap-4"
+              onSubmit={form.handleSubmit((data) =>
+                handlePasswordReset(data.newPassword),
+              )}
+            >
+              <FormField
+                control={form.control}
+                name="newPassword"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormControl>
+                      <Input
+                        placeholder="New Password"
+                        type="password"
+                        className="h-13 w-full"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="confirmPassword"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormControl>
+                      <Input
+                        placeholder="Confirm Password"
+                        type="password"
+                        className="h-13 w-full"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Button
+                className="h-13 w-full"
+                disabled={isLoading || !form.formState.isValid}
+              >
+                Reset Password
+              </Button>
+            </form>
+          </Form>
+        </EmptyContent>
+      </Empty>
+    </div>
+  );
+}
---
+++ src/app/reset-password/page.tsx
@@ -0,0 +1,29 @@
+"use client";
+import { useRouter, useSearchParams } from "next/navigation";
+import { useEffect } from "react";
+import { toast } from "sonner";
+import SendResetLink from "./SendResetLink";
+import SetNewPassword from "./SetNewPassword";
+
+export default function Page() {
+  const searchParams = useSearchParams();
+  const router = useRouter();
+
+  const token = searchParams.get("token");
+  const error = searchParams.get("error");
+
+  useEffect(() => {
+    if (error) {
+      setTimeout(() => {
+        toast.error("There was an error while loading reset password form");
+        router.replace("/reset-password");
+      }, 100);
+    }
+  }, [error]);
+
+  if (token) {
+    return <SetNewPassword token={token} />;
+  } else {
+    return <SendResetLink />;
+  }
+}
