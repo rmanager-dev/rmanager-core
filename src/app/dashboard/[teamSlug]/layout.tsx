2058ad5aeb8cae8486d18de401492e123c535d2b
OverDsh <overdshdev@gmail.com>
2026-01-28 16:19:01 +0100

    feat: create members page

--- src/app/dashboard/[teamSlug]/layout.tsx
+++ src/app/dashboard/[teamSlug]/layout.tsx
@@ -7,7 +7,15 @@ export default function Layout({ children }: React.PropsWithChildren) {
   return (
     <>
       <TeamSidebar />
-      <SidebarInset>{children}</SidebarInset>
+      <SidebarInset>
+        <main className="w-full overflow-auto">
+          <div className="w-full px-2 py-10 md:px-10 lg:px-15 xl:px-20 flex justify-center">
+            <div className="flex w-full flex-col items-center gap-6 max-w-5xl">
+              {children}
+            </div>
+          </div>
+        </main>
+      </SidebarInset>
     </>
   );
 }
--- src/app/dashboard/[teamSlug]/members/page.tsx
+++ src/app/dashboard/[teamSlug]/members/page.tsx
@@ -1,3 +1,22 @@
+"use client";
+import { DataTable } from "@/src/components/DataTable";
+import { memberColumn } from "./MemberColumn";
+import { useMembers } from "@/src/hooks/useMember";
+
 export default function Page() {
-  return <span>Members</span>;
+  const { data: members, isLoading } = useMembers();
+
+  return (
+    <>
+      <span className="w-full text-left text-lg font-semibold">Members</span>
+      <DataTable
+        columns={memberColumn}
+        data={members ?? []}
+        emptyString="No members found"
+        loading={isLoading}
+        searchBoxPlaceholder="Search members"
+        searchBoxTarget="email"
+      />
+    </>
+  );
 }
