2a46c18f99cdcc54930aba1e0ae6770d16e76a27
OverDsh <overdshdev@gmail.com>
2026-02-04 15:59:01 +0100

    fix: remove type from RobloxCredentialInfoSchema

--- src/lib/types/roblox-credentials-types.ts
+++ src/lib/types/roblox-credentials-types.ts
@@ -19,14 +19,13 @@ export const RobloxCredentialInfo = {
 export type RobloxCredentialInfo = InferDrizzleSelect<
   typeof RobloxCredentialInfo
 >;
-export const RobloxCredentialInfoSchema: z.ZodType<RobloxCredentialInfo> =
-  z.object({
-    name: z
-      .string()
-      .min(3, { error: "Name must be at least 3 characters" })
-      .max(32, { error: "Name must be at most 32 characters" }),
-    key: z.string().max(2048, { error: "Key must be at most 2048 characters" }),
-  });
+export const RobloxCredentialInfoSchema = z.object({
+  name: z
+    .string()
+    .min(3, { error: "Name must be at least 3 characters" })
+    .max(32, { error: "Name must be at most 32 characters" }),
+  key: z.string().max(2048, { error: "Key must be at most 2048 characters" }),
+});
 
 export const RobloxCredentialRenameSchema = z.object({
   name: z
