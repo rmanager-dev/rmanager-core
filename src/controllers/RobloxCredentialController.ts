35fbb2b36c16fde90af03b319e99c6d78be9ed67
OverDsh <overdshdev@gmail.com>
2026-02-04 15:12:14 +0100

    feat: create frontend hooks for RobloxCredentialService

---
+++ src/controllers/RobloxCredentialController.ts
@@ -0,0 +1,34 @@
+import {
+  RobloxCredential,
+  RobloxCredentialInfo,
+} from "../lib/types/roblox-credentials-types";
+import { fetcher } from "../lib/utils/api-utils";
+
+export const RobloxCredentialController = {
+  link: (teamId: string, data: RobloxCredentialInfo) =>
+    fetcher<RobloxCredential>(`/api/teams/${teamId}/roblox-credentials`, {
+      method: "POST",
+      body: JSON.stringify(data),
+    }),
+
+  delete: (teamId: string, credId: string) =>
+    fetcher<RobloxCredential>(
+      `/api/teams/${teamId}/roblox-credentials/${credId}`,
+      { method: "DELETE" },
+    ),
+
+  rename: (teamId: string, credId: string, newName: string) =>
+    fetcher<RobloxCredential>(
+      `/api/teams/${teamId}/roblox-credentials/${credId}`,
+      { method: "PATCH", body: JSON.stringify({ name: newName }) },
+    ),
+
+  rotate: (teamId: string, credId: string, newKey: string) =>
+    fetcher(`/api/teams/${teamId}/roblox-credentials/${credId}`, {
+      method: "POST",
+      body: JSON.stringify({ key: newKey }),
+    }),
+
+  list: (teamId: string) =>
+    fetcher<RobloxCredential[]>(`/api/teams/${teamId}/roblox-credentials`),
+};
