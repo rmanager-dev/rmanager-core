ec31b19fd0da308b96baed821b18c2a5340a4651
OverDsh <overdshdev@gmail.com>
2026-02-04 14:26:34 +0100

    feat: create api routes for RobloxCredentialService

---
+++ src/app/api/teams/[teamId]/roblox-credentials/[credentialId]/route.ts
@@ -0,0 +1,146 @@
+import { auth } from "@/src/lib/auth";
+import {
+  RobloxCredentialRenameSchema,
+  RobloxCredentialRotateSchema,
+} from "@/src/lib/types/roblox-credentials-types";
+import { AccessDenied, ErrorToNextResponse } from "@/src/lib/utils/errors";
+import { RobloxCredentialsService } from "@/src/services/RobloxCredentialsService";
+import { headers } from "next/headers";
+import { NextResponse } from "next/server";
+
+interface Context {
+  params: Promise<{
+    teamId: string;
+    credentialId: string;
+  }>;
+}
+
+export async function DELETE(req: Request, context: Context) {
+  const { teamId, credentialId } = await context.params;
+  if (!teamId) {
+    return NextResponse.json({ error: "Team ID is required" }, { status: 400 });
+  }
+  if (!credentialId) {
+    return NextResponse.json(
+      { error: "Credential ID is required" },
+      { status: 400 },
+    );
+  }
+
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+  if (!session) {
+    return ErrorToNextResponse(AccessDenied);
+  }
+
+  try {
+    const deletedCredential =
+      await RobloxCredentialsService.DeleteRobloxCredential(
+        session.user.id,
+        teamId,
+        credentialId,
+      );
+    return NextResponse.json(deletedCredential);
+  } catch (error) {
+    return ErrorToNextResponse(error);
+  }
+}
+
+export async function PATCH(req: Request, context: Context) {
+  const { teamId, credentialId } = await context.params;
+  if (!teamId) {
+    return NextResponse.json({ error: "Team ID is required" }, { status: 400 });
+  }
+  if (!credentialId) {
+    return NextResponse.json(
+      { error: "Credential ID is required" },
+      { status: 400 },
+    );
+  }
+
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+  if (!session) {
+    return ErrorToNextResponse(AccessDenied);
+  }
+
+  let body;
+  try {
+    body = await req.json();
+  } catch {
+    return NextResponse.json({ error: "Invalid json body" }, { status: 400 });
+  }
+
+  let validatedData;
+  try {
+    validatedData = RobloxCredentialRenameSchema.parse(body);
+  } catch {
+    return NextResponse.json(
+      { error: "Invalid body request format" },
+      { status: 400 },
+    );
+  }
+
+  try {
+    const renamedCredential =
+      await RobloxCredentialsService.RenameRobloxCredential(
+        session.user.id,
+        teamId,
+        credentialId,
+        validatedData.name,
+      );
+    return NextResponse.json(renamedCredential);
+  } catch (error) {
+    return ErrorToNextResponse(error);
+  }
+}
+
+export async function POST(req: Request, context: Context) {
+  const { teamId, credentialId } = await context.params;
+  if (!teamId) {
+    return NextResponse.json({ error: "Team ID is required" }, { status: 400 });
+  }
+  if (!credentialId) {
+    return NextResponse.json(
+      { error: "Credential ID is required" },
+      { status: 400 },
+    );
+  }
+
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+  if (!session) {
+    return ErrorToNextResponse(AccessDenied);
+  }
+
+  let body;
+  try {
+    body = await req.json();
+  } catch {
+    return NextResponse.json({ error: "Invalid json body" }, { status: 400 });
+  }
+
+  let validatedData;
+  try {
+    validatedData = RobloxCredentialRotateSchema.parse(body);
+  } catch {
+    return NextResponse.json(
+      { error: "Invalid body request format" },
+      { status: 400 },
+    );
+  }
+
+  try {
+    await RobloxCredentialsService.RotateRobloxCredential(
+      session.user.id,
+      teamId,
+      credentialId,
+      validatedData.key,
+    );
+  } catch (error) {
+    return ErrorToNextResponse(error);
+  }
+}
--- src/lib/types/roblox-credentials-types.ts
+++ src/lib/types/roblox-credentials-types.ts
@@ -1,5 +1,6 @@
 import { roblox_credentials } from "@/src/db/schema/roblox_credentials";
 import { InferDrizzleSelect } from "../utils";
+import z from "zod";
 
 export const RobloxCredentialSelect = {
   id: roblox_credentials.id,
@@ -18,3 +19,22 @@ export const RobloxCredentialInfo = {
 export type RobloxCredentialInfo = InferDrizzleSelect<
   typeof RobloxCredentialInfo
 >;
+export const RobloxCredentialInfoSchema: z.ZodType<RobloxCredentialInfo> =
+  z.object({
+    name: z
+      .string()
+      .min(3, { error: "Name must be at least 3 characters" })
+      .max(32, { error: "Name must be at most 32 characters" }),
+    key: z.string().max(2048, { error: "Key must be at most 2048 characters" }),
+  });
+
+export const RobloxCredentialRenameSchema = z.object({
+  name: z
+    .string()
+    .min(3, { error: "Name must be at least 3 characters" })
+    .max(32, { error: "Name must be at most 32 characters" }),
+});
+
+export const RobloxCredentialRotateSchema = z.object({
+  key: z.string().max(2048, { error: "Key must be at most 2048 characters" }),
+});
