3954bcfef6fcd5cb6c1a68d08773db5559728835
OverDsh <overdshdev@gmail.com>
2026-04-06 14:36:27 +0200

    feat: remove displayName from team types and consolidate zod schemas

--- src/lib/types/team-types.ts
+++ src/lib/types/team-types.ts
@@ -1,5 +1,6 @@
 import { user, team_member, team } from "@/src/db/schema";
 import { InferDrizzleSelect } from "../utils";
+import z from "zod";
 
 export type TeamRole = typeof team_member.$inferSelect.role;
 
@@ -15,7 +16,6 @@ export type TeamMember = InferDrizzleSelect<typeof TeamMemberSelect>;
 export const TeamSelect = {
   id: team.id,
   name: team.name,
-  displayName: team.displayName,
   slug: team.slug,
   ownerId: team.ownerId,
   createdAt: team.createdAt,
@@ -28,3 +28,18 @@ export const UserTeamSelect = {
   role: team_member.role,
 };
 export type UserTeam = InferDrizzleSelect<typeof UserTeamSelect>;
+
+
+export const CreateTeamSchema = z.object({
+  name: z
+    .string()
+    .min(3, { error: "Team name must be at least 3 characters" })
+    .max(32, { error: "Team name must be at most 32 characters" }),
+});
+
+export const RenameTeamSchema = z.object({
+  name: z
+    .string()
+    .min(3, { error: "Name must be at least 3 characters" })
+    .max(32, { error: "Name must be at most 32 characters" }),
+});
