b5fb99239817a75215a4c97727bf0328c57115f2
OverDsh <overdshdev@gmail.com>
2026-04-09 20:55:18 +0200

    feat: add organization plugin to client and server better auth config

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,6 +1,7 @@
 import { twoFactorClient } from "better-auth/plugins";
+import { organizationClient } from "better-auth/client/plugins";
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
-  plugins: [twoFactorClient()],
+  plugins: [twoFactorClient(), organizationClient()],
 });
