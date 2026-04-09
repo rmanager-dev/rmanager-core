d58177ff09e27f96d2d04545c20242effc4300c1
OverDsh <overdshdev@gmail.com>
2026-04-09 20:55:41 +0200

    feat: create organization permissions layer

--- src/lib/utils/auth-utils.ts
+++ src/lib/utils/auth-utils.ts
@@ -1,6 +1,10 @@
 import { db } from "@/src/db";
 import { user } from "@/src/db/schema";
 import { eq } from "drizzle-orm";
+import { auth } from "../auth";
+import { NextResponse } from "next/server";
+import { OrgPermissions } from "../permissions";
+import { AccessDenied, ErrorToNextResponse } from "./api-utils";
 
 export async function CheckUserExist(UserId: string): Promise<boolean> {
   const userProfile = await db.query.user.findFirst({
@@ -8,4 +12,28 @@ export async function CheckUserExist(UserId: string): Promise<boolean> {
     columns: { id: true },
   });
   return !!userProfile;
+}
+
+export async function requireOrgPermission(
+  req: Request,
+  organizationId: string,
+  permissions?: OrgPermissions,
+) {
+  const headers = req.headers;
+  const session = await auth.api.getSession({ headers });
+
+  if (!session) {
+    throw AccessDenied;
+  }
+
+  const { success } = await auth.api.hasPermission({
+    headers,
+    body: { organizationId, permissions },
+  });
+
+  if (!success) {
+    throw AccessDenied;
+  } else {
+    return session;
+  }
 }
