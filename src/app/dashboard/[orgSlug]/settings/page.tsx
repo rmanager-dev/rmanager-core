ab164f473fa7a5a9c806908803d33cb05bda5b89
OverDsh <overdshdev@gmail.com>
2026-04-16 19:01:10 +0200

    feat: add OrganizationSlug settings component

--- src/app/dashboard/[orgSlug]/settings/page.tsx
+++ src/app/dashboard/[orgSlug]/settings/page.tsx
@@ -1,12 +1,14 @@
 "use client";
 import OrganizationDangerZone from "./OrganizationDangerZone";
 import OrganizationName from "./OrganizationName";
+import OrganizationSlug from "./OrganizationSlug";
 
 export default function Page() {
   return (
     <>
       <span className="w-full text-left text-lg font-semibold">Organization Settings</span>
       <OrganizationName />
+      <OrganizationSlug />
       <OrganizationDangerZone />
     </>
   );
