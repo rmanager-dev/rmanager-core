840b9926cd2e29108ae3d6a59da1cb4f569bccaf
OverDsh <overdshdev@gmail.com>
2026-04-18 14:26:44 +0200

    feat: remove api routes from web

--- rmanager-web/src/app/api/auth/[...all]/route.ts
+++
@@ -1,4 +0,0 @@
-import { auth } from "@rmanager/shared/lib/auth"; // path to your auth file
-import { toNextJsHandler } from "better-auth/next-js";
-
-export const { POST, GET } = toNextJsHandler(auth);
