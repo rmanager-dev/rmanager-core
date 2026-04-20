23fe2bd986f091b3b4ff26bc9b1247b9ff5e0ea7
OverDsh <overdshdev@gmail.com>
2026-04-20 14:00:18 +0200

    feat: add docker support

--- rmanager-api/src/index.ts
+++ rmanager-api/src/index.ts
@@ -1,5 +1,5 @@
 import { serve } from "@hono/node-server";
 import app from "./app";
 
-serve({ fetch: app.fetch, port: Number(process.env.PORT) ?? 3001 });
-console.log(`[HONO]: API ready on port ${process.env.PORT ?? 3001}`);
+serve({ fetch: app.fetch, port: Number(process.env.API_PORT) ?? 3001 });
+console.log(`[HONO]: API ready on port ${process.env.API_PORT ?? 3001}`);
